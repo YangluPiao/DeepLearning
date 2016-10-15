9ae69bfc091fc960a718d5fa77f6fa002e1efe81
Yanglu Piao <yp2419@columbia.edu>
2016-10-15 17:23:14 -0400

    push test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import theano
+from theano import tensor as T
+
+#Initialize symbolic variables. Also could be vector, matrices, or even much higher demisonal tensors.
+a=T.scalar()
+b=T.scalar()
+
+#Define symbolic expression
+y=a*b
+
+#Compile a function
+multiply=theano.function(inputs=[a,b],outputs=y)
+
+#Use the function on numeric data.
+print (multiply(3,2)) #6
+print (multiply(4,5)) #20
